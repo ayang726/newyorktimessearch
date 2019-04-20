bfbc6201e31c3e8148124e6f6c3e1042e646fdc1
kerianne16 <kerianne116@gmail.com>
2019-04-20 13:43:34 -0400

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //var articleToSearch = $("#search-article").val();
-var articleToSearch = $('search-article').val();
-var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${articleToSearch}&page=1&sort=newest&api-key=4Gfcr4VEPhIGGaDJ8A3gjBJbDRs80nLr`;
+var articleToSearch = $('#search-article').val();
+var pubData = $('#pub-year').val();
+var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=election&pub_year=${pubDate}&page=1&sort=newest&api-key=4Gfcr4VEPhIGGaDJ8A3gjBJbDRs80nLr`;
 
 function getData() {
     $.ajax({
@@ -10,3 +11,4 @@ function getData() {
         console.log(response);
     })
 }
+getData();
