c67eaad379942704fb3f621d2e0e8680b443d11a
kerianne16 <kerianne116@gmail.com>
2019-04-20 13:35:31 -0400

    App.js

---
+++ app.js
@@ -0,0 +1,10 @@
+//var articleToSearch = $("#search-article").val();
+var articleToSearch = $('search-article').val();
+var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${articleToSearch}&page=1&sort=newest&api-key=4Gfcr4VEPhIGGaDJ8A3gjBJbDRs80nLr`;
+
+$.ajax({
+    url: url,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+})
